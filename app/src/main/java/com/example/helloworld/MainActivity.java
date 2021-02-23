a660e1d48cd04ce06bae6fbfbbaa3188c481b688
108590027 <t108590027@ntut.org.tw>
2021-02-23 18:00:44 +0800

    HW1

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    //use class name as tag
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,  "Test creating the URL...");
+    }
+}
