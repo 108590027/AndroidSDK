df8d157a61b5a6c8eed6ac9bbbfe1bf2a3060d27
108590027 <t108590027@ntut.org.tw>
2021-04-13 18:05:32 +0800

    HW7-1

--- 108590027_HW7_1/app/src/main/java/com/example/a108590027_hw7_1/MainActivity.java
+++ 108590027_HW7_1/app/src/main/java/com/example/a108590027_hw7_1/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.a108590027_hw7_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void donut_click(View view){
+        Intent intent = new Intent(this, DonutActivity.class);
+        startActivity(intent);
+    }
+
+    public void froyo_click(View view){
+        Intent intent = new Intent(this, FroyoActivity.class);
+        startActivity(intent);
+    }
+
+    public void ice_click(View view){
+        Intent intent = new Intent(this, IceActivity.class);
+        startActivity(intent);
     }
 }
