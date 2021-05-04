52317c09364abdfc67236dcfd199c97c374a7b33
108590027 <t108590027@ntut.org.tw>
2021-05-04 17:44:35 +0800

    HW9-1 creaated

---
+++ 108590027_HW9_1/app/src/main/java/com/example/a108590027_hw9_1/WordListAdapter.java
@@ -0,0 +1,68 @@
+package com.example.a108590027_hw9_1;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder>{
+    private final LinkedList<String> mWordList;
+    private final LayoutInflater mInflater;
+
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    //class WordViewHolder extends RecyclerView.ViewHolder{
+        public final TextView wordView;
+        final WordListAdapter mAdapter;
+
+
+        public WordViewHolder(View itemView, WordListAdapter adapter) {
+            super(itemView);
+            wordView = itemView.findViewById(R.id.word);
+            this.mAdapter = adapter;
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Get the position of the item that was clicked.
+            int mPosition = getLayoutPosition();
+            // Use that to access the affected item in mWordList.
+            String element = mWordList.get(mPosition);
+            // Change the word in the mWordList.
+            mWordList.set(mPosition, "Clicked! " + element);
+            // Notify the adapter, that the data has changed so it can
+            // update the RecyclerView to display the data.
+            mAdapter.notifyDataSetChanged();
+
+        }
+    }
+
+    public WordListAdapter(Context context, LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.wordlist_item, parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordListAdapter.WordViewHolder holder, int position) {
+        String mCurrent_content = mWordList.get(position);
+        holder.wordView.setText(mCurrent_content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+}
